750ab9bee8e24be1f20d1d4d4b8f2f8596c65267
divijesh <divijeshvarma@pm.me>
2021-02-18 13:27:11 +0530

    comparision

---
+++ ComparisionsOperators.py
@@ -0,0 +1,26 @@
+# Comparisions
+
+num1 = 3
+num2 = 4
+
+print(num1 == num2)
+
+print(num1 != num2)
+
+print(num1 > num2)
+
+print(num1 < num2)
+
+print(num1 >= num2)
+
+print(num1 <= num2)
+
+# casting is to cast from string to integer
+
+num1 = '100'
+num2 = '200'
+
+num1 = int(num1)
+num2 = int(num2)
+
+print(num2 + num1)
