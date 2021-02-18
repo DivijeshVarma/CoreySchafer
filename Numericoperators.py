85e4e59dfbeee9dff7b707a80c5c260f0d65d8ca
divijesh <divijeshvarma@pm.me>
2021-02-18 13:04:18 +0530

    numeric operators

---
+++ Numericoperators.py
@@ -0,0 +1,33 @@
+# Arithematic operators
+
+num = 3
+num2 = 3.12
+print(type(num))
+print(type(num2))
+
+# division
+print(3 / 2)
+
+# floor division
+print(3 // 2)
+
+# exponent
+print(3 ** 2)
+
+# modulus
+print(3 % 2)
+
+num = 1
+
+num += 1
+print(num)
+
+num = 2
+
+num *= 10
+print(num)
+
+print(round(3.65))
+print(round(3.65, 1))
+
+print(abs(-3))
