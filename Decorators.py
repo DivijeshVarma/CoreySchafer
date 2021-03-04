254531c8d799bfbe7f4208074c387ce81f823ac7
divijesh <divijeshvarma@pm.me>
2021-03-04 10:48:20 +0530

    decoratorss

--- Decorators.py
+++ Decorators.py
@@ -22,3 +22,67 @@ def print_str():
 
 
 print(print_str())
+
+
+#########################
+# functions in python are objects which means we can pass them around
+# args and kwargs will accept all args passed in
+
+def func(f):
+    def wrapper(*args, **kwargs):
+        f(*args, **kwargs)
+        print('i am func')
+
+    return wrapper
+
+
+@func
+def func2(x, y):
+    print(x, y)
+
+
+@func
+def func3():
+    print('i am func3')
+
+
+# we just want to call function2 and decorator. rather than
+# call func and pass func2, we can @func
+
+func2(5, 6)
+func3()
+# x = func(func2)
+# print(x.__name__)
+# x()
+# x = func(func3)
+# print(x.__name__)
+# x()
+
+
+#########################
+
+def func(f):
+    def wrapper(*args, **kwargs):
+        re = f(*args, **kwargs)
+        print('i am func')
+        return re
+    return wrapper
+
+
+@func
+def func2(x, y):
+    print(x)
+    return y
+
+
+@func
+def func3():
+    print('i am func3')
+
+
+# we just want to call function2 and decorator. rather than
+# call func and pass func2, we can @func
+
+print(func2(5, 6))
+
+func3()
